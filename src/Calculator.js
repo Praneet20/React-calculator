a7adf6f68471da7f9d1bf1766d8e796ae56235aa
Ramandeep Singh <ramandeepsingh@Ramandeeps-MacBook-Pro-3.local>
2020-03-05 23:19:18 -0500

    This is a calculator

---
+++ src/Calculator.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class Calculator extends Component {
+
+
+    constructor(props) {
+      super(props)
+      this.state = {
+         count: 0
+      }
+    }
+
+    increement(){
+        this.setState({
+            count: this.state.count+1
+       })
+    }
+
+  Decrement()
+  {
+      this.setState(
+      {
+          count: this.state.count-1
+      })
+  }
+    
+
+  render() {
+    return (
+      <div>
+        <div className="Design">Count = {this.state.count}</div>
+          <button onClick ={()=> this.increement()}>Increement</button>
+          <br/>
+          <button onClick={()=> this.Decrement()}>Decrement</button>
+        
+      </div>
+    )
+  }
+}
